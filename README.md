3ed93b3166d1506d944fff0b101c327f7f3b5e14
Calvin Woo <calvinwoo32@gmail.com>
2023-05-28 19:23:53 -0700

    Baseline lazyvim with paperclip color scheme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dotfiles
 
 `./install`
+
+The above should install a nerd font. Ensure it is set as the font in the terminal.
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require'colorizer'.setup()
-require('leap').add_default_mappings()
-
-vim.opt.background = "light"
-vim.cmd "colorscheme PapercolorSlim"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
